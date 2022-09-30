accdb075c5395a19d66452717d6ef76b3e7a19d3
Ilya Golovin <me@ilscc.ru>
2022-09-30 20:41:51 +0300

    Add i3 specific scripts

---
+++ Linux/i3wm/scripts/toggle_gaps.sh
@@ -0,0 +1,20 @@
+#! /bin/env bash
+
+tmp_file="/tmp/.i3-gaps"
+
+if ! [ -z "$1" ]
+then
+ touch $tmp_file
+fi
+
+if [ ! -e "$tmp_file" ]; then
+    touch $tmp_file
+    i3-msg gaps inner all set 0
+    i3-msg gaps outer all set 0
+    pkill picom
+else
+    rm $tmp_file
+    i3-msg restart
+    sleep 0.5
+    picom --daemon --config ~/.config/i3/picom.conf
+fi
