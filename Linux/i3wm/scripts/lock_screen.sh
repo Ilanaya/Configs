9e3d64781be46fdab8d66ed4d024fafd3a59c1d2
Ilya Golovin <me@ilscc.ru>
2022-11-27 04:27:12 +0300

    feat(betterlockscreen)

---
+++ Linux/i3wm/scripts/lock_screen.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+ convert image.jpg -resize $(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/') RGB:- | i3lock --raw $(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/'):rgb --image /dev/stdin
+ #reference https://github.com/Raymo111/i3lock-color
