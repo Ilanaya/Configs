a0c746f1f81a37a60bd4d26f9a9dd8ef89119b6f
Ilya Golovin <me@ilanaya.ru>
2025-03-12 22:35:52 +0300

    feat: add some vscode setup

--- vscode/keybindings-normallize.js
+++
@@ -1,8 +0,0 @@
-const { modifyJsonFile } = require('modify-json-file')
-const path = require('node:path')
-
-modifyJsonFile(path.join(__dirname, 'keybindings.json'), keybindingsArr => {
-    return keybindingsArr.filter(({ command }) => {
-        return !command.startsWith('-')
-    })
-})
